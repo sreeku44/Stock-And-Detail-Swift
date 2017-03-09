c73e8b68ef208a7f59c0b76601d5c78b14d71888
sreeku44 <2sreeku44@gmail.com>
2017-03-09 11:45:09 -0600

    Initial Commit

---
+++ Stock And  Detail Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stock And  Detail Swift
+//
+//  Created by Sreekala Santhakumari on 3/9/17.
+//  Copyright © 2017 Klas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
